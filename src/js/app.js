70b235906c7a10019933c4bb295b01e60dea47f2
Dominick Peluso <peluso.dominick@gmail.com>
2015-10-14 20:06:48 -0400

    Start implementing Angular in template

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  var fizzbuzz = 0;
-  $('#plus-btn').click(function(){
-    fizzbuzz++;
-    $('#fizzbuzz').html(fizzbuzz);
-  });
-  $('#fizzbuzz').html(fizzbuzz);
-});
